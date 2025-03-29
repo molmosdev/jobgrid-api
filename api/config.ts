8d9808841e9cae7a820266b4a2f926ae00257d20
molmosdev <hello@molmos.dev>
2025-03-29 12:45:44 +0100

    Update production flag to use environment variable for configuration

--- api/config.ts
+++ api/config.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase";
 
 export default {
-  production: true,
+  production: Deno.env.get("PRODUCTION")!,
   database: createClient(
     Deno.env.get("SUPABASE_URL")!,
     Deno.env.get("SUPABASE_ANON_KEY")!,
