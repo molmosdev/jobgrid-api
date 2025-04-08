fc303712daa5ba0d82d3a2145a1e1fddff38cf6f
molmosdev <hello@molmos.dev>
2025-04-08 23:41:11 +0200

    fix: change cors middleware to return promise instead of await

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -5,7 +5,7 @@ export const corsMiddleware: MiddlewareHandler = async (c, next) => {
   const origin = c.env.CLIENT_STATIC_URL;
   c.set("domain", new URL(c.env.CLIENT_STATIC_URL).hostname);
 
-  await cors({
+  return cors({
     origin,
     credentials: true,
   })(c, next);
